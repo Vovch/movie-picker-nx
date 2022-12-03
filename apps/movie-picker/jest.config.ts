390f419af54d4779e5fc5bdda0df52c6a44a820f
Vovch <Vovch00@gmail.com>
2022-12-03 18:51:17 +0300

    Remove commented code in jest.config.ts

--- apps/movie-picker/jest.config.ts
+++ apps/movie-picker/jest.config.ts
@@ -12,7 +12,6 @@ export default {
   coverageDirectory: '../../coverage/apps/movie-picker',
   transform: {
     '^.+\\.(ts|mjs|js|html|svg)$': 'jest-preset-angular',
-    // '^.+\\.svg$': '',
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
