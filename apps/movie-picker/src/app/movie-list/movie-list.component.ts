9c50b03ab302ab0dcfcbe0ee26935e4e6d8f07a0
Vovch <Vovch00@gmail.com>
2022-12-04 18:40:40 +0300

    Add sorting function

--- apps/movie-picker/src/app/movie-list/movie-list.component.ts
+++ apps/movie-picker/src/app/movie-list/movie-list.component.ts
@@ -2,43 +2,65 @@ import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
 import {IMovie} from "@movie-picker/api-interfaces";
 
 @Component({
-    selector: 'movie-picker-list',
-    templateUrl: './movie-list.component.html',
-    styleUrls: ['./movie-list.component.less'],
+  selector: 'movie-picker-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.less'],
 })
 export class MovieListComponent implements OnChanges {
-    @Input() movies: IMovie[] = [];
-    @Output() selectMovie = new EventEmitter<IMovie>();
+  @Input() movies: IMovie[] = [];
+  @Output() selectMovie = new EventEmitter<IMovie>();
 
-    columns: (keyof IMovie)[] = [];
-    search = '';
-    filteredMovies: IMovie[] = [];
+  columns: (keyof IMovie)[] = [];
+  search = '';
+  filteredMovies: IMovie[] = [];
+  currentSort: null | keyof IMovie = null;
 
-    ngOnChanges() {
-        if (this.movies.length) {
-            this.columns = Object.keys(this.movies[0]).filter((key) => key !== 'id') as (keyof IMovie)[];
-            this.filterMovies();
-        } else {
-          this.filteredMovies = [];
+  ngOnChanges() {
+    if (this.movies.length) {
+      this.columns = Object.keys(this.movies[0]).filter((key) => key !== 'id') as (keyof IMovie)[];
+      this.filterMovies();
+    } else {
+      this.filteredMovies = [];
+    }
+  }
+
+  filterMovies() {
+    const caseInsensitiveSearch = this.search.toLowerCase();
+
+    this.filteredMovies = this.movies.filter((movie) => {
+      return Object.entries(movie).some(
+        ([key, value]) => key !== 'id' && String(value).toLowerCase().includes(caseInsensitiveSearch),
+      );
+    });
+  }
+
+  handleSearchChange($event: Event) {
+    this.search = ($event.target as HTMLInputElement).value;
+    this.filterMovies();
+  }
+
+  handleRowClick(movie: IMovie) {
+    this.selectMovie.emit(movie);
+  }
+
+  handleSort(column: keyof IMovie) {
+    if (column === this.currentSort) {
+      this.movies.reverse();
+    } else {
+      this.movies.sort((a, b) => {
+        let sort = 0;
+
+        if (a[column] > b[column]) {
+          sort = 1;
+        } else if (a[column] < b[column]) {
+          sort = -1;
         }
+
+        return sort
+      })
     }
 
-    filterMovies() {
-        const caseInsensitiveSearch = this.search.toLowerCase();
-
-        this.filteredMovies = this.movies.filter((movie) => {
-            return Object.entries(movie).some(
-                ([key, value]) => key !== 'id' && String(value).toLowerCase().includes(caseInsensitiveSearch),
-            );
-        });
-    }
-
-    handleSearchChange($event: Event) {
-        this.search = ($event.target as HTMLInputElement).value;
-        this.filterMovies();
-    }
-
-    handleRowClick(movie: IMovie) {
-      this.selectMovie.emit(movie);
-    }
+    this.currentSort = column
+    this.filterMovies();
+  }
 }
