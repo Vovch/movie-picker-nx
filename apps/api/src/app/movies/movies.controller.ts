674bd8036253578f33cceceee7205705047ac309
Vladimir Mironov <vovch00@gmail.com>
2024-11-10 20:16:41 +0300

    test

--- apps/api/src/app/movies/movies.controller.ts
+++ apps/api/src/app/movies/movies.controller.ts
@@ -1,29 +1,13 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MovieList } from '../database/schemas/movieList.schema';
-import { IMovie } from 'libs/api-interfaces/src/lib/api-interfaces';
 
 @Controller('movies')
 export class MoviesController {
-  constructor(private readonly moviesService: MoviesService) {}
+    constructor(private readonly moviesService: MoviesService) {}
 
-  @Get()
-  async findAll(): Promise<MovieList> {
-    return await this.moviesService.findAll();
-  }
-
-  @Post()
-  async createMovie(@Body() movie: IMovie): Promise<MovieList> {
-    return await this.moviesService.createMovie(movie);
-  }
-
-  @Put(':id')
-  async updateMovie(@Param('id') id: number, @Body() movie: IMovie): Promise<MovieList> {
-    return await this.moviesService.updateMovie(id, movie);
-  }
-
-  @Delete(':id')
-  async deleteMovie(@Param('id') id: number): Promise<MovieList> {
-    return await this.moviesService.deleteMovie(id);
-  }
+    @Get()
+    async findAll(): Promise<MovieList> {
+        return await this.moviesService.findAll();
+    }
 }
